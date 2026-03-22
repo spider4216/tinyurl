35df385d953caa8bdc59c55a501febe3c064390f
Yuriy Sirotenko <yosirotenko@gmail.com>
2026-03-22 09:46:49 +0500

    Revert "generate url handler test"
    This reverts commit f72cd5df3aef3ace63fbcaedcd814781c4ffb215.

--- internal/service/service.go
+++ internal/service/service.go
@@ -3,21 +3,18 @@ package service
 import (
 	"crypto/rand"
 	"encoding/base64"
+
+	"github.com/spider4216/tinyurl/internal/repository"
 )
 
-type Store interface {
-	Insert(k string, v string)
-	Get(k string) string
-}
-
-func New(repo Store) Service {
+func New(repo repository.Repository) Service {
 	return Service{
 		repo: repo,
 	}
 }
 
 type Service struct {
-	repo Store
+	repo repository.Repository
 }
 
 func (s Service) GenerateId() string {
