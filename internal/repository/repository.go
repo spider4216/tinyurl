ccb155a3d16665b0fc840a156b501d06bb53543f
Yuriy Sirotenko <yosirotenko@gmail.com>
2026-03-29 18:39:01 +0500

    mutex

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,5 +1,7 @@
 package repository
 
+import "sync"
+
 func New(store map[string]string) Repository {
 	return Repository{
 		store: store,
@@ -8,13 +10,20 @@ func New(store map[string]string) Repository {
 
 type Repository struct {
 	store map[string]string
+	mu    sync.RWMutex
 }
 
-func (r Repository) Insert(k string, v string) {
+func (r *Repository) Insert(k string, v string) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
 	r.store[k] = v
 }
 
-func (r Repository) Get(k string) string {
+func (r *Repository) Get(k string) string {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
 	v, ok := r.store[k]
 
 	if !ok {
