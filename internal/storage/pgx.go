7c4129bfa2e92edb452ec8cd0ecc59971e152fa0
Yuriy Sirotenko <yosirotenko@gmail.com>
2026-04-27 13:59:22 +0500

    checking map ok

--- internal/storage/pgx.go
+++ internal/storage/pgx.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
@@ -72,15 +73,27 @@ func NewPgxStorage(con string) (*PgxStorage, error) {
 func (db *PgxStorage) Save(data []byte) error {
 	vals := map[string]string{}
 
-	// Приходится делать unmarshal поскольку на уровне драйвера нужно понимать
+	// Приходится делать unmarshal поскольку на уровне store нужно понимать
 	// схему таблицы
 	if err := json.Unmarshal(data, &vals); err != nil {
 		return err
 	}
 
+	short, ok := vals["short_url"]
+
+	if !ok {
+		return fmt.Errorf("unrecognize columns")
+	}
+
+	origin, ok := vals["original_url"]
+
+	if !ok {
+		return fmt.Errorf("unrecognize columns")
+	}
+
 	sql := "INSERT INTO urls (short, original) VALUES ($1, $2)"
 
-	_, err := db.Con.Exec(sql, vals["short_url"], vals["original_url"])
+	_, err := db.Con.Exec(sql, short, origin)
 
 	log.Println("Error here", err)
 
