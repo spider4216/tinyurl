70906eb1a9d503072a02a6bc5922ab567d8ab7a3
Yuriy Sirotenko <yosirotenko@gmail.com>
2026-06-27 07:52:28 +0500

    audit test

---
+++ internal/audit/audit_test.go
@@ -0,0 +1,57 @@
+package audit
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type auditSliceObserver struct {
+	Data [][]byte
+}
+
+func (aso *auditSliceObserver) GetID() string {
+	return "test_audit_slice_observer"
+}
+
+func (aso *auditSliceObserver) Update(data Body) error {
+	raw, err := json.Marshal(data)
+
+	if err != nil {
+		return err
+	}
+
+	aso.Data = append(aso.Data, raw)
+
+	return nil
+}
+
+func TestAudit(t *testing.T) {
+	event := NewAuditEvent()
+
+	ob := &auditSliceObserver{}
+
+	event.Register(ob)
+
+	data := Body{
+		TS:     time.Now(),
+		Action: ShortenAction,
+		UserID: "test1",
+		URL:    "http://test.loc",
+	}
+
+	err := event.Notify(data)
+	require.NoError(t, err)
+
+	assert.Len(t, ob.Data, 1)
+	raw := ob.Data[0]
+
+	model := Body{}
+
+	err = json.Unmarshal(raw, &model)
+
+	assert.Equal(t, data.Action, model.Action)
+}
