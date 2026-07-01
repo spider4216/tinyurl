2b60cc1f3458ec2acb70bc16abf801b78b003994
Yuriy.Sirotenko <yuriy_sirotenko@Yuriy.Sirotenko>
2026-07-01 09:35:35 +0500

    doc example audit

--- internal/audit/audit_test.go
+++ internal/audit/audit_test.go
@@ -2,6 +2,7 @@ package audit
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 	"time"
 
@@ -54,4 +55,32 @@ func TestAudit(t *testing.T) {
 	require.NoError(t, err)
 
 	assert.Equal(t, data.Action, model.Action)
+}
+
+func ExampleAuditEvent_Notify() {
+	// Создаем событие
+	event := NewAuditEvent()
+
+	// Готовим Observer в котором фигурирует логика оповещения
+	// Можно создать свой Observer и реализовать свою логику, например
+	// Вести аудит в БД. Главное чтобы пользовательский Observer
+	// реализовал интерфейс Observer
+	ob := &auditSliceObserver{}
+
+	// Регистрируем Observer
+	event.Register(ob)
+
+	// Готовим тело аудита
+	data := Body{
+		TS:     time.Now(),
+		Action: ShortenAction,
+		UserID: "test1",
+		URL:    "http://test.loc",
+	}
+
+	if err := event.Notify(data); err != nil {
+		// Обработать ошибку, например залогировать
+	}
+
+	fmt.Println("Оповещение прошло успешно")
 }
