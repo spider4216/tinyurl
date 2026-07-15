1fc4d420caed6cb4402f881cc70af5b8b4c2c4af
Yuriy Sirotenko <yosirotenko@gmail.com>
2026-07-15 10:10:18 +0500

    small refactor

--- internal/models/url_item.go
+++ internal/models/url_item.go
@@ -1,15 +1,9 @@
 package models
 
-import "fmt"
-
 // UrlItem единая модель на которую мапятся данные из разных Store.
 type UrlItem struct {
 	ShortUrl    string `json:"short_url"`
 	OriginarUrl string `json:"original_url"`
 	IsDeleted   bool   `json:"deleted_at"`
 	UserId      string `json:"user_id"`
-}
-
-func (v *UrlItem) Reset() {
-	fmt.Println("reset")
 }
