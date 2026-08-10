7f998e184abaf541ff5b6ec7d98989a4f76279fe
Yuriy.Sirotenko <yuriy_sirotenko@Yuriy.Sirotenko>
2026-08-10 14:45:43 +0500

    expand url

--- internal/grpc/server.go
+++ internal/grpc/server.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 
 	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 
 	"github.com/spider4216/tinyurl/internal/audit"
@@ -46,16 +48,15 @@ func (s *ShortenerServer) ShortenURL(ctx context.Context, in *pb.URLShortenReque
 	userId := s.service.GetUserIdFromCtx(ctx)
 
 	if userId == "" {
-		err := errors.New("cannot convert user id to string")
-		s.logger.Error(err)
-		return nil, err
+		msg := "cannot convert user id to string"
+		return nil, status.Error(codes.Internal, msg)
 	}
 
 	err := s.service.StoreData(ctx, id, string(in.GetUrl()), userId)
 
 	if err != nil && !s.service.IsErrAsDuplicate(err) {
-		s.logger.Errorf("store error: %s", zap.Error(err))
-		return nil, err
+		s.logger.Errorf("store error: %s", err)
+		return nil, status.Error(codes.Internal, err.Error())
 	}
 
 	// Если дубликат, то тогда извлекаем по значению
@@ -65,9 +66,9 @@ func (s *ShortenerServer) ShortenURL(ctx context.Context, in *pb.URLShortenReque
 		id, err = s.service.GetShortByOrigin(ctx, in.GetUrl())
 
 		if err != nil {
-			s.logger.Errorf("store error: %s", zap.Error(err))
+			s.logger.Errorf("store error: %s", err)
 
-			return nil, err
+			return nil, status.Error(codes.Internal, err.Error())
 		}
 	}
 
@@ -83,7 +84,39 @@ func (s *ShortenerServer) ShortenURL(ctx context.Context, in *pb.URLShortenReque
 }
 
 func (s *ShortenerServer) ExpandURL(ctx context.Context, in *pb.URLExpandRequest) (*pb.URLExpandResponse, error) {
-	return nil, nil
+	ctx, cancel := context.WithTimeout(ctx, s.cfg.CtxTimeout)
+
+	defer cancel()
+
+	url, err := s.service.GetUrl(ctx, in.GetId())
+	var deletedError service.DeletedUrlError
+
+	if errors.As(err, &deletedError) {
+		err := errors.New("url was deleted")
+		s.logger.Error(err)
+		return nil, status.Error(codes.NotFound, err.Error())
+	}
+
+	if err != nil {
+		s.logger.Errorf("get data error: %s", err)
+		return nil, status.Error(codes.NotFound, err.Error())
+	}
+
+	if url == "" {
+		err := errors.New("url not found")
+		s.logger.Error(err)
+		return nil, status.Error(codes.NotFound, err.Error())
+	}
+
+	userId := s.service.GetUserIdFromCtx(ctx)
+
+	s.service.AuditNotify(audit.FollowAction, userId, url)
+
+	var resp pb.URLExpandResponse
+
+	resp.SetResult(url)
+
+	return &resp, nil
 }
 
 func (s *ShortenerServer) ListUserURLs(ctx context.Context, in *emptypb.Empty) (*pb.UserURLsResponse, error) {
